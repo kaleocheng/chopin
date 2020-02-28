631394e4bee502e5ca73f014e82d631632402b66
Kaleo <kaleocheng@gmail.com>
2020-02-28 12:39:31 +0800

    Fix index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,5 @@
 function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === 'complete' || document.readyState === 'interactive') {
-    // call on next available tick
-        setTimeout(fn, 1)
-    } else {
-        document.addEventListener('DOMContentLoaded', fn)
-    }
+  document.addEventListener('DOMContentLoaded', fn)
 }
 
 function isNumeric(num){
